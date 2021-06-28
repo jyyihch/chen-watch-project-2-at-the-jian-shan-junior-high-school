48bc5f2de90120daed0b6f80f480d8f9c10cf6cd
jyyihch <cichen.tw@gmail.com>
2021-06-28 17:06:55 +0800

    還有兩個bug，調整，還有55分的時候，燈不會滅

--- main.ts
+++ main.ts
@@ -1,32 +1,34 @@
-let min_hand = 0
-let angle = 0
-let five_x: number[] = []
-let five_y: number[] = []
-let list2: number[] = []
-let list3: number[] = []
 input.onButtonPressed(Button.A, function () {
-    if (min_hand >= 11) {
-        min_hand = 0
-    } else {
+    if (min_hand < 11) {
         min_hand += 1
+    } else {
+        min_hand = 0
     }
 })
+let five_count = 0
+let min_hand = 0
+let angle = 0.062832
+let five_x = [2, 4, 4, 0, 0]
+let five_y = [2, 0, 4, 4, 0]
+let list2 = [2, 3, 4, 4, 4, 3, 2, 1, 0, 0, 0, 1]
+let list3 = [0, 0, 1, 2, 3, 4, 4, 4, 3, 2, 1, 0]
 basic.forever(function () {
-    angle = 0.062832
-    five_x = [2, 4, 4, 0, 0]
-    five_y = [2, 0, 4, 4, 0]
-    list2 = [2, 3, 4, 4, 4, 3, 2, 1, 0, 0, 0, 1]
-    list3 = [0, 0, 1, 2, 3, 4, 4, 4, 3, 2, 1, 0]
-    for (let min_hand = 0; min_hand <= 11; min_hand++) {
-        led.plotBrightness(list2[min_hand], list3[min_hand], 255)
-        for (let five_count = 0; five_count <= 4; five_count++) {
-            for (let index = 0; index < 12; index++) {
-                for (let index = 0; index <= 49; index++) {
-                    led.plotBrightness(five_x[five_count], five_y[five_count], 255 * Math.sin((index + 1) * angle))
-                    basic.pause(1)
-                }
+    led.plotBrightness(list2[min_hand], list3[min_hand], 255)
+    if (five_count < 4) {
+        five_count += 1
+        for (let index = 0; index < 12; index++) {
+            for (let index = 0; index <= 49; index++) {
+                led.plotBrightness(five_x[five_count], five_y[five_count], 255 * Math.sin((index + 1) * angle))
+                basic.pause(1)
             }
         }
-        led.toggle(list2[min_hand], list3[min_hand])
+    } else {
+        five_count = 0
+        if (min_hand < 11) {
+            led.toggle(list2[min_hand], list3[min_hand])
+            min_hand += 1
+        } else {
+            min_hand = 0
+        }
     }
 })

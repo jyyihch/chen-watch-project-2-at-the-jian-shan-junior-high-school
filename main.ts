9345385919055f02fdd6d1a617585b9be9a59332
jyyihch <cichen.tw@gmail.com>
2021-06-30 17:10:34 +0800

    點是時針，octo是分針

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    led.plotBrightness(octo_x[hour_hand], octo_y[hour_hand], 0)
     if (hour_hand == min_hand) {
-        led.plotBrightness(list2[min_hand], list3[min_hand], 255)
+        led.plotBrightness(list2[min_hand], list3[min_hand], 85)
     } else {
         led.plotBrightness(list2[hour_hand], list3[hour_hand], 0)
     }
@@ -10,8 +9,7 @@ input.onButtonPressed(Button.A, function () {
     } else {
         hour_hand = 0
     }
-    led.plotBrightness(list2[hour_hand], list3[hour_hand], 85)
-    led.plotBrightness(octo_x[hour_hand], octo_y[hour_hand], 170)
+    led.plotBrightness(list2[hour_hand], list3[hour_hand], 255)
 })
 input.onButtonPressed(Button.AB, function () {
     if (five_count == 0) {
@@ -27,8 +25,9 @@ input.onButtonPressed(Button.AB, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
+    led.plotBrightness(octo_x[min_hand], octo_y[min_hand], 0)
     if (hour_hand == min_hand) {
-        led.plotBrightness(list2[hour_hand], list3[hour_hand], 85)
+        led.plotBrightness(list2[hour_hand], list3[hour_hand], 255)
     } else {
         led.plotBrightness(list2[min_hand], list3[min_hand], 0)
     }
@@ -37,7 +36,8 @@ input.onButtonPressed(Button.B, function () {
     } else {
         min_hand = 0
     }
-    led.plotBrightness(list2[min_hand], list3[min_hand], 255)
+    led.plotBrightness(list2[min_hand], list3[min_hand], 85)
+    led.plotBrightness(octo_x[min_hand], octo_y[min_hand], 128)
 })
 let five_count = 0
 let min_hand = 0
@@ -57,26 +57,26 @@ octo_x = [2, 2, 3, 3, 3, 2, 2, 2, 1, 1, 1, 2]
 octo_y = [1, 1, 2, 2, 2, 3, 3, 3, 2, 2, 2, 1]
 basic.forever(function () {
     led.plotBrightness(2, 2, 170)
-    led.plotBrightness(octo_x[hour_hand], octo_y[hour_hand], 170)
-    led.plotBrightness(list2[hour_hand], list3[hour_hand], 85)
-    led.plotBrightness(list2[min_hand], list3[min_hand], 255)
-    for (let index = 0; index < 12; index++) {
+    led.plotBrightness(octo_x[min_hand], octo_y[min_hand], 128)
+    led.plotBrightness(list2[min_hand], list3[min_hand], 85)
+    led.plotBrightness(list2[hour_hand], list3[hour_hand], 255)
+    for (let index = 0; index < 1; index++) {
         for (let index2 = 0; index2 <= 49; index2++) {
             led.plotBrightness(five_x[five_count], five_y[five_count], 255 * Math.sin((index2 + 1) * angle))
-            basic.pause(100)
+            basic.pause(1)
         }
     }
     if (five_count < 4) {
         five_count += 1
     } else {
         five_count = 0
-        led.toggle(list2[min_hand], list3[min_hand])
+        led.plotBrightness(list2[min_hand], list3[min_hand], 0)
+        led.plotBrightness(octo_x[min_hand], octo_y[min_hand], 0)
         if (min_hand < 11) {
             min_hand += 1
         } else {
             min_hand = 0
-            led.plotBrightness(list2[hour_hand], list3[hour_hand], 0)
-            led.plotBrightness(octo_x[hour_hand], octo_y[hour_hand], 0)
+            led.toggle(list2[hour_hand], list3[hour_hand])
             if (hour_hand < 11) {
                 hour_hand += 1
             } else {
